65c83dc82d00b60dd044e5035234be2e5155989b
tungdt <daototungne@gmail.com>
2023-10-19 20:34:36 +0700

    update

--- dist/scripts.js
+++ dist/scripts.js
@@ -856,7 +856,7 @@ $(document).ready(function() {
 	};
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Oct 26, 2023 09:30:00").getTime();
+		var countDownDate = new Date("Oct 29, 2023 15:30:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
